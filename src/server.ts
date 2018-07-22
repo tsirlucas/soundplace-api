b331ae535566ea9bc90e8e56fcf138857581e3f4
Lucas Correia <tsirlucas@gmail.com>
2018-07-21 23:37:16 -0300

    fix(dbPem): add dbPem before start server (#3)

--- src/server.ts
+++ src/server.ts
@@ -1,18 +1,30 @@
+import {environment} from 'config';
+import {writeFile} from 'fs';
+import {promisify} from 'util';
+
 import app from 'src';
+
+const fs_writeFile = promisify(writeFile);
 
 interface AddressInfoWithPort {
   port: number;
 }
 
-const httpServer = app.listen(process.env.PORT || 3000, (error: Error) => {
-  if (error) {
-    console.error(error);
-  } else {
-    const address = httpServer.address() as AddressInfoWithPort;
-    console.info(
-      `==> 🌎 Listening on ${address.port}. Open up http://localhost:${
-        address.port
-      }/ in your browser.`,
-    );
-  }
-});
+fs_writeFile('postgres.pem', environment.secrets.dbPem)
+  .then(() => {
+    const httpServer = app.listen(process.env.PORT || 3000, (error: Error) => {
+      if (error) {
+        console.error(error);
+      } else {
+        const address = httpServer.address() as AddressInfoWithPort;
+        console.info(
+          `==> 🌎 Listening on ${address.port}. Open up http://localhost:${
+            address.port
+          }/ in your browser.`,
+        );
+      }
+    });
+  })
+  .catch((error: Error) => {
+    console.log(error);
+  });
