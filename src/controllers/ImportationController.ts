baa949990a0221f91b5bcb4b63f7a26687112b1d
tsirlucas <tsirlucas@gmail.com>
2018-08-04 22:00:08 -0300

    fix(import): only on res.send when finish imports

--- src/controllers/ImportationController.ts
+++ src/controllers/ImportationController.ts
@@ -18,7 +18,6 @@ export class SubscriptionController {
   private async importUser(_req: Request, res: Response) {
     const user = await YoutubeDataService.getInstance().getUserData(res.locals.userId);
     await YoutubeUserUpdater.getInstance().setUser(user);
-    res.send();
   }
 
   private async importPlaylistTracks(userId: string, playlists: Playlist[]) {
@@ -39,12 +38,12 @@ export class SubscriptionController {
     const playlists = await YoutubeDataService.getInstance().getUserPlaylists(userId);
     await YoutubePlaylistUpdater.getInstance().setPlaylists(playlists, userId);
     await this.importPlaylistTracks(userId, playlists);
-    res.send();
   }
 
   public importData = async (req: Request, res: Response) => {
     await this.importUser(req, res);
     await this.importPlaylists(req, res);
     await YoutubeUserUpdater.getInstance().setImporting(res.locals.userId, false);
+    res.send();
   };
 }
